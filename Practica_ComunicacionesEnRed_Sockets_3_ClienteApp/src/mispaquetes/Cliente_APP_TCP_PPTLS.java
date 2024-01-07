7c821b70dd5a59b271799e8491159788734695c8
Miguel750VS <miguel.vizcaino750@gmail.com>
2024-01-07 12:12:49 +0100

    Signed-off-by: Miguel750VS <miguel.vizcaino750@gmail.co

---
+++ Practica_ComunicacionesEnRed_Sockets_3_ClienteApp/src/mispaquetes/Cliente_APP_TCP_PPTLS.java
@@ -0,0 +1,165 @@
+package mispaquetes;
+
+
+import java.awt.EventQueue;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class Cliente_APP_TCP_PPTLS extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8670939934650654871L;
+	
+	
+/**
+ * 
+ *
+ * @throws UnknownHostException
+ * @throws IOException
+ */
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ImageIcon icon = new ImageIcon("img/Icon.png");
+					Cliente_APP_TCP_PPTLS frame = new Cliente_APP_TCP_PPTLS();
+					frame.setVisible(true);
+					frame.setTitle("(C) Miguel v 1.0");
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					frame.setBounds(400, 200, 450, 300);
+					frame.setIconImage(icon.getImage());
+					String Host = "localhost";//Red publica con puerto abierto / Ipv4 de red local / localhost
+					int Puerto = 7500;// puerto remoto
+					Socket Cliente = null;
+
+					String juego = "";
+					int contadorW = 0, contadorL = 0;
+
+					try {
+						Cliente = new Socket(Host, Puerto);
+
+						// CREO FLUJO DE SALIDA => SERVIDOR
+						DataOutputStream flujoSalida = new DataOutputStream(Cliente.getOutputStream());
+
+						// CREO FLUJO DE ENTRADA <= SERVIDOR
+						DataInputStream flujoEntrada = new DataInputStream(Cliente.getInputStream());
+
+						Boolean lbAcabado = false;
+						String entrada = "";
+						while (!lbAcabado) {
+							do {
+								System.out.println();
+								// entrada de una cadena
+								juego = JOptionPane.showInputDialog(frame,
+										"Quieres Jugar ¿PIEDRA, PAPEL, TIJERAS, LAGARTO O SPOCK?", "-_JUEGA PPTLS_-",
+										JOptionPane.QUESTION_MESSAGE, new ImageIcon(icon.getImage()), null, "").toString()
+										.toUpperCase();
+								if (juego.equals("TIJERA")) {
+
+									juego = "TIJERAS";
+								}
+							} while (!juego.equals("PIEDRA") && !juego.equals("PAPEL") && !juego.equals("TIJERAS")
+									&& !juego.equals("LAGARTO") && !juego.equals("SPOCK"));
+
+							// EL JUGADOR 1 ENVIA SU ELECCION
+							flujoSalida.writeUTF(juego);
+
+							JOptionPane.showMessageDialog(frame, "Esperando al Rival", "Enter para continuar...",
+									JOptionPane.PLAIN_MESSAGE);
+
+							// Capturo lo que me manda el servidor, Primero el numero de Ronda
+							entrada = flujoEntrada.readUTF();
+							juego = "\t" + entrada + "\n";
+							// Capturo lo que me manda el servidor, La jugada contraria para mostrarla en el
+							// cliente
+							entrada = flujoEntrada.readUTF();
+							juego += "\tEl otro Jugador jugo: " + entrada + "\n";
+							// Capturo lo que me manda el servidor,El mensaje de enfrentamiento
+							entrada = flujoEntrada.readUTF();
+							juego += "\t" + entrada + "\n";
+							// Capturo lo que me manda el servidor, El resultado
+							entrada = flujoEntrada.readUTF();
+							juego += "\t---------------\n";
+
+							// Miro a ver si he ganado perdido o empatado
+							if (entrada.contains("W")) {
+								contadorW++;
+								juego += "\tHe ganado. Yo voy: " + contadorW + " y el otro: " + contadorL + "\n";
+							}
+
+							else if (entrada.contains("L")) {
+								contadorL++;
+								juego += "\tHe perdido. Yo voy: " + contadorW + " y el otro: " + contadorL + "\n";
+							} else if (entrada.contains("E")) {
+								juego += "\tHe empatado. Yo voy: " + contadorW + " y el otro: " + contadorL + "\n";
+							}
+
+							// Compruebo si he ganado o perdido
+							if (contadorL == 3 || contadorW == 3) {
+								lbAcabado = true;
+							} else {
+								juego += "\t---------------";
+								JOptionPane.showMessageDialog(frame, juego, "Resultado", JOptionPane.PLAIN_MESSAGE);
+							}
+
+						}
+						juego += "\t---Partida Terminada---";
+						JOptionPane.showMessageDialog(frame, juego,
+								"Resultado Final: " + ((contadorW == 3) ? "Enhorabuena!!" : "Suerte la Proxima vez"),
+								JOptionPane.PLAIN_MESSAGE);
+						// CERRAR STREAMS Y SOCKETS
+						flujoEntrada.close();
+						flujoSalida.close();
+						Cliente.close();
+
+
+					} catch (ConnectException e) {
+						JOptionPane.showMessageDialog(frame, "El servidor no esta abierto", "Error", JOptionPane.ERROR_MESSAGE);
+
+					} catch (SocketException e) {
+						JOptionPane.showMessageDialog(frame,
+								"Tiraron del Cable o la sala esta Ocupada, Espere la siguiente Partida", "Error",
+								JOptionPane.ERROR_MESSAGE);
+
+					} catch (EOFException e) {
+						JOptionPane.showMessageDialog(frame,
+								"Tiraron del Cable o la sala esta Ocupada, Espere la siguiente Partida", "Error",
+								JOptionPane.ERROR_MESSAGE);
+
+					} catch (NullPointerException e) {
+						try {
+							Cliente.close();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						System.exit(0);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}// main
+
+	
+
+}
---
+++ Practica_ComunicacionesEnRed_Sockets_3_ServidorApp/src/mispaquetes/Server_APP_TCP_PPTLS.java
@@ -0,0 +1,271 @@
+package mispaquetes;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Server_APP_TCP_PPTLS {
+	
+	static int contadorW = 0;
+	static int contadorL = 0;
+	static int round = 1;
+
+	
+	/**
+	 * 
+	 * 
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		int puerto = 7500;// puerto remoto
+		ServerSocket servidor=null;
+		Socket jugador1=null;
+		Socket jugador2=null;
+		DataInputStream flujoEntrada_1=null;
+		DataInputStream flujoEntrada_2=null;
+		DataOutputStream flujoSalida_1=null;
+		DataOutputStream flujoSalida_2=null;
+		do {
+			try {
+		servidor = new ServerSocket(puerto);
+
+		System.out.println("Esperando conexiones...");
+
+		jugador1 = servidor.accept();
+		System.out.println("Jugador 1 conectado desde: " + jugador1.getInetAddress().getHostAddress());
+		jugador2 = servidor.accept();
+		System.out.println("Jugador 2 conectado desde: " + jugador2.getInetAddress().getHostAddress());
+		// CREO FLUJO DE ENTRADA AL CLIENTE
+		flujoEntrada_1 = new DataInputStream(jugador1.getInputStream());
+
+		// CREO FLUJO DE SALIDA AL CLIENTE
+		flujoSalida_1 = new DataOutputStream(jugador1.getOutputStream());
+
+		// CREO FLUJO DE ENTRADA AL CLIENTE
+		flujoEntrada_2 = new DataInputStream(jugador2.getInputStream());
+
+		// CREO FLUJO DE SALIDA AL CLIENTE
+		flujoSalida_2 = new DataOutputStream(jugador2.getOutputStream());
+
+		Boolean lbAcabado = false;
+		String juego1, juego2;
+
+		while (!lbAcabado) {
+			// Capturo lo que me manda el Jugador 1
+			juego1 = flujoEntrada_1.readUTF();
+
+			// Capturo lo que me manda el Jugador 2
+			juego2 = flujoEntrada_2.readUTF();
+
+			// Procesar la jugada y enviar respuesta al cliente
+			procesarJugada(juego1, juego2, flujoSalida_1, flujoSalida_2);
+
+			// Miro a ver si el servidor ganó o perdió
+
+			System.out.println("Jugador 1 va: " + contadorW + " y el Jugador 2: " + contadorL);
+
+			// Compruebo si el servidor ganó o perdió
+			if (contadorL == 3 || contadorW == 3) {
+				lbAcabado = true;
+			}else {
+				System.out.println("---------------");
+			}
+		}
+		System.out.println("---Partida Terminada---");
+		System.out.println("(Copyright Sam Kass)");
+
+			}catch(EOFException e) {
+				System.out.println("(1)- Nueva Partida");
+
+			}catch(SocketException e) {
+				System.out.println("(2)- Nuevo Comienzo");
+
+			}
+		// CERRAR STREAMS Y SOCKETS
+		servidor.close();
+		flujoEntrada_1.close();
+		flujoSalida_1.close();
+		flujoEntrada_2.close();
+		flujoSalida_2.close();
+		jugador1.close();
+		jugador2.close();
+		reiniciarContadores();
+		System.out.println("(3)- Play Again");
+		}while(servidor.isClosed());
+	}
+	
+
+	/**
+	 * 
+	 * @param eleccion1
+	 * @param eleccion2
+	 * @param salida1
+	 * @param salida2
+	 * @throws IOException
+	 * @throws EOFException
+	 * @throws SocketException
+	 */
+	public static void procesarJugada(String eleccion1, String eleccion2, DataOutputStream salida1,
+			DataOutputStream salida2) throws IOException,EOFException,SocketException{
+		int pos = 0;
+		// Muestro Ronda y la envio a Jugadores
+		String enfrentamiento = "---[Ronda " + round + "]---";
+		salida1.writeUTF(enfrentamiento);
+		salida2.writeUTF(enfrentamiento);
+		System.out.println(enfrentamiento);
+		round++;
+		// Muestro Elecciones de los Jugadores y mando la del oponente
+		salida1.writeUTF(eleccion2);
+		salida2.writeUTF(eleccion1);
+		System.out.println("Jugador 1 Jugo: " + eleccion1 + " y el Jugador 2 Jugo: " + eleccion2);
+		// Compruebo si ganan empatan o pierden, primero envio mensaje de enfrentamiento
+		// y despues del resultado.
+		if (eleccion1.equals(eleccion2)) {
+			enfrentamiento = "HAN EMPATADO";
+			salida1.writeUTF(enfrentamiento);
+			salida2.writeUTF(enfrentamiento);
+			salida1.writeUTF("E");
+			salida2.writeUTF("E");
+			System.out.println(enfrentamiento);
+		} else {
+			switch (eleccion1) {
+			//
+			case "PIEDRA":
+				switch (eleccion2) {
+				case "PAPEL":
+					enfrentamiento = "Papel envuelve piedra";
+					pos = 1;
+					break;
+				case "TIJERAS":
+					enfrentamiento = "Piedra aplasta tijeras";
+					pos = 0;
+					break;
+				case "LAGARTO":
+					enfrentamiento = "Piedra aplasta lagarto";
+					pos = 0;
+					break;
+				case "SPOCK":
+					enfrentamiento = "Spock desintegra piedra";
+					pos = 1;
+					break;
+				}
+				break;
+			//
+			case "PAPEL":
+				switch (eleccion2) {
+				case "PIEDRA":
+					enfrentamiento = "Papel envuelve piedra";
+					pos = 0;
+					break;
+				case "TIJERAS":
+					enfrentamiento = "Tijeras cortan papel";
+					pos = 1;
+					break;
+				case "LAGARTO":
+					enfrentamiento = "Lagarto se come papel";
+					pos = 1;
+					break;
+				case "SPOCK":
+					enfrentamiento = "Papel desaprueba a Spock";
+					pos = 0;
+					break;
+				}
+				break;
+			//
+			case "TIJERAS":
+				switch (eleccion2) {
+				case "PIEDRA":
+					enfrentamiento = "Piedra aplasta tijeras";
+					pos = 1;
+					break;
+				case "PAPEL":
+					enfrentamiento = "Tijeras cortan papel";
+					pos = 0;
+					break;
+				case "LAGARTO":
+					enfrentamiento = "Tijeras decapitan lagarto";
+					pos = 0;
+					break;
+				case "SPOCK":
+					enfrentamiento = "Spock aplasta tijeras";
+					pos = 1;
+					break;
+				}
+				break;
+			//
+			case "LAGARTO":
+				switch (eleccion2) {
+				case "PIEDRA":
+					enfrentamiento = "Piedra aplasta lagarto";
+					pos = 1;
+					break;
+				case "PAPEL":
+					enfrentamiento = "Lagarto se come papel";
+					pos = 0;
+					break;
+				case "TIJERAS":
+					enfrentamiento = "Tijeras decapitan lagarto";
+					pos = 1;
+					break;
+				case "SPOCK":
+					enfrentamiento = "Lagarto envenena a Spock";
+					pos = 0;
+					break;
+				}
+				break;
+			//
+			case "SPOCK":
+				switch (eleccion2) {
+				case "PIEDRA":
+					enfrentamiento = "Spock desintegra piedra";
+					pos = 0;
+					break;
+				case "PAPEL":
+					enfrentamiento = "Papel desaprueba a Spock";
+					pos = 1;
+					break;
+				case "TIJERAS":
+					enfrentamiento = "Spock aplasta tijeras";
+					pos = 0;
+					break;
+				case "LAGARTO":
+					enfrentamiento = "Lagarto envenena a Spock";
+					pos = 1;
+					break;
+				}
+				break;
+			}
+			// Mando mensaje enfrentamiento y lo muestro tambien en Servidor
+			salida1.writeUTF(enfrentamiento);
+			salida2.writeUTF(enfrentamiento);
+			System.out.println(enfrentamiento);
+			// Sumo al ganador, enseño y mando resultado
+			if (pos == 0) {
+				contadorW++;
+				salida1.writeUTF("W");
+				salida2.writeUTF("L");
+				System.out.println("JUGADOR 1 GANA");
+			} else {
+				salida1.writeUTF("L");
+				salida2.writeUTF("W");
+				contadorL++;
+				System.out.println("JUGADOR 2 GANA");
+			}
+
+		}
+		System.out.println("---------------");
+	}
+	
+	public static void reiniciarContadores() {
+		contadorW = 0;
+		contadorL = 0;
+		round = 1;
+	}
+}
